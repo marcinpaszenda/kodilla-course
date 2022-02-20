23f640f72e9de4e794b64dcf7ff0c0de41f8364b
Marcin Paszenda <marcinpaszenda@gmail.com>
2022-02-20 16:48:32 +0100

    TDD: book directory test suite with Mock

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/library/BookLibrary.java
@@ -0,0 +1,30 @@
+package com.kodilla.testing.library;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BookLibrary {
+    private LibraryDatabase libraryDatabase;
+    public Map<LibraryUser, List<Book>> booksMap = new HashMap<>();
+
+    public BookLibrary(LibraryDatabase libraryDatabase) {
+        this.libraryDatabase = libraryDatabase;
+    }
+
+    public List<Book> listBooksWithCondition(String titleFragment) {
+        List<Book> bookList = new ArrayList<Book>();
+        if (titleFragment.length() < 3) return bookList;
+        List<Book> resultList = libraryDatabase.listBooksWithCondition(titleFragment);
+        if (resultList.size() > 20) return bookList;
+        bookList = resultList;
+        return bookList;
+
+    }
+
+    public List<Book> listBooksInHandsOf(LibraryUser libraryUser) {
+        return booksMap.get(libraryUser);
+
+    }
+}
