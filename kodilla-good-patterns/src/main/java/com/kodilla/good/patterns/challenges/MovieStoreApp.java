c7cad8b5ed6f036a8f83c1fdfc1215303e82f451
Marcin Paszenda <marcinpaszenda@gmail.com>
2022-04-20 00:17:32 +0200

    13.1 Stream, Map (Good patterns)

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStoreApp.java
@@ -0,0 +1,16 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.stream.Collectors;
+
+public class MovieStoreApp {
+
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        String result = movieStore.getMovies().values().stream()
+                .flatMap(s -> s.stream())
+                .collect(Collectors.joining(" ! "));
+        System.out.println(result);
+
+
+    }
+}
