344dd14c605bb14d8f041b3f3c55a4685064e8e3
Marcin Paszenda <marcinpaszenda@gmail.com>
2022-05-28 02:00:48 +0200

    15.3 Strategy

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -0,0 +1,47 @@
+package com.kodilla.patterns.strategy.social;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserTestSuite {
+
+    @Test
+    void testDefaultSharingStrategies() {
+
+        // Given
+        User mike = new Millenials("Mike Kowalsky");
+        User bryan = new YGeneration("Bryan Nowak");
+        User jescia = new ZGeneration("Jesica Poziomka");
+
+        // When
+        String mikeUse = mike.sharePost();
+        System.out.println("Mike use: " + mikeUse);
+        String bryanUse = bryan.sharePost();
+        System.out.println("Bryan use: " + bryanUse);
+        String jesicaUse = jescia.sharePost();
+        System.out.println("Jesica use: " + jesicaUse);
+
+        // Then
+        assertEquals("Facebook", mikeUse);
+        assertEquals("Twitter", bryanUse);
+        assertEquals("Snapchat", jesicaUse);
+    }
+
+    @Test
+    void testIndividualSharingStrategy() {
+
+        //Given
+        User mike = new Millenials("Mike Kowalsky");
+
+        // When
+        String mikeUse = mike.sharePost();
+        System.out.println("Mike use: " + mikeUse);
+        mike.setPublishingStrategy(new SnapchatPublisher());
+        mikeUse = mike.sharePost();
+        System.out.println("Now Mike use: " + mikeUse);
+
+        // Then
+        assertEquals("Snapchat", mikeUse);
+    }
+}
