87bfc1d0534040a43ce03bd1c9ecb32e07134108
Marcin Paszenda <marcinpaszenda@gmail.com>
2022-06-06 01:25:07 +0200

    16.6. Komunikacja aplikacji z bazą danych – JDBC

---
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -0,0 +1,37 @@
+package com.kodilla.jdbc;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public enum DbManager {
+
+    INSTANCE;
+
+    private Connection conn;
+
+    DbManager() {
+        Properties connectionProps = new Properties();
+        connectionProps.put("user", "root");
+        connectionProps.put("password", "ptaki_lataja_kluczem");
+        try {
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/kodilla_course" +
+                            "?serverTimezone=Europe/Warsaw" +
+                            "&useSSL=False",
+                    connectionProps);
+        } catch (SQLException e) {
+            throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    public static DbManager getInstance() {
+        return INSTANCE;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+}
